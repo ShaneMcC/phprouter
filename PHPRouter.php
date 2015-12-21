66f28e1a1c1b9887554a15806916c7d5364913b7
Shane Mc Cormack <shanemcc@gmail.com>
2015-12-21 10:54:02 +0000

    IOSXR

--- PHPRouter.php
+++ PHPRouter.php
@@ -7,6 +7,7 @@
 	// Routers
 	require_once(dirname(__FILE__) . '/Router.php');
 	require_once(dirname(__FILE__) . '/CiscoRouter.php');
+	require_once(dirname(__FILE__) . '/IOSXRRouter.php');
 	require_once(dirname(__FILE__) . '/ArborOS.php');
 
 ?>
