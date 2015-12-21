f7af4f7ba4574bb2cfc1234d8249adce876cd049
Shane Mc Cormack <shanemcc@gmail.com>
2015-12-21 10:53:55 +0000

    IOSXR

---
+++ IOSXRRouter.php
@@ -0,0 +1,23 @@
+<?php
+	/**
+	 * Class to interact with an iosxr  router, over ssh.
+	 */
+	class IOSXRRouter extends CiscoRouter {
+		/* {@inheritDoc} */
+		function getPrefixList($name, $type = 'ipv4') {
+			$type = ($type == 'ipv4' ? 'ip' : 'ipv6');
+			$data = $this->exec('show run prefix-set ' . $name);
+			$entries = array();
+			foreach (explode("\n", $data) as $line) {
+				if (preg_match('#([0-9a-f.:]+/[0-9]+[^,]+)#i', trim($line), $m)) {
+					$entries[] = 'permit ' . strtolower(trim($m[1]));
+				}
+			}
+			return $entries;
+		}
+
+		function enable($password = '') {
+			/* No-Op */
+		}
+	}
+?>
